e70649b57e37b0f7adc3eb02011d1772d4ea8beb
jesuslab135 <jefeing.esteban@gmail.com>
2025-10-17 20:32:14 -0700

    Login-Register y Landing

---
+++ src/pages/login-register/Login_Register.jsx
@@ -0,0 +1,11 @@
+import LoginRegister from "../../components/Login_Register_components/LoginRegister";
+
+const Login_Register = () => {
+    return (
+        <>
+            <LoginRegister/>
+        </>
+    )
+}
+
+export default Login_Register;
