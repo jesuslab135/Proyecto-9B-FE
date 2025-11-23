ec8b14baa52242806c5b4422a1496509186f4152
gagor02 <gagor0202@gmail.com>
2025-11-22 23:44:04 -0800

    22/11/2025 users styles and conditions, reports generated mockup

--- src/components/admin/usuarios_crud_components/Table_users.jsx
+++ src/components/admin/usuarios_crud_components/Table_users.jsx
@@ -4,44 +4,100 @@ import Pagination from "../../../components/Global_components/Pagination";
 
 const Table_users = () => {
 
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
   
   const itemsPerPage = 5;
   const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        
-        const response = await fetch("http://127.0.0.1:8000/api/usuarios/");
-        const json = await response.json();
-        setUsers(json);
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const usersData = [
+    {
+      id: 1,
+      name: "Juan Pérez",
+      role: "Consumidor",
+      email: "user@gmail.com", 
+      cellphone: "555-1234",
+      status: "Inactivo"
+    },
+    {
+      id: 2,
+      name: "Juan Pérez",
+      role: "Administrador",
+      email: "user@gmail.com", 
+      cellphone: "555-1234",
+      status: "Activo"
+    }, 
+    {
+      id: 3,
+      name: "Juan Pérez",
+      role: "Administrador",
+      email: "user@gmail.com", 
+      cellphone: "555-1234",
+      status: "Activo"
+    }, 
+    {
+      id: 4,
+      name: "Juan Pérez",
+      role: "Administrador",
+      email: "user@gmail.com", 
+      cellphone: "555-1234",
+      status: "Activo"
+    }, 
+    {
+      id: 5,
+      name: "Juan Pérez",
+      role: "Administrador",
+      email: "user@gmail.com", 
+      cellphone: "555-1234",
+      status: "Activo"
+    }, 
+    {
+      id: 6,
+      name: "Juan Pérez",
+      role: "Administrador",
+      email: "user@gmail.com", 
+      cellphone: "555-1234",
+      status: "Activo"
+    }, 
+    {
+      id: 7,
+      name: "Juan Pérez",
+      role: "Administrador",
+      email: "user@gmail.com", 
+      cellphone: "555-1234",
+      status: "Activo"
+    }
+  ]
 
-    fetchUsers();
-    // console.log('q se imprime:', json);
-  }, []);
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     try {
+  //       const response = await fetch("http://127.0.0.1:8000/api/usuarios/");
+  //       const json = await response.json();
+  //       setUsers(json);
+  //     } catch (error) {
+  //       console.error("Error fetching users:", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-  if (loading) {
-    return <div>Cargando usuarios...</div>;
-  }
+  //   fetchUsers();
+  //   // console.log('q se imprime:', json);
+  // }, []);
 
   // Math.ceil redondea hacia arriba al entero más cercano
-  const totalPages = Math.ceil(users.length / itemsPerPage);
+  const totalPages = Math.ceil(usersData.length / itemsPerPage);
 
   // Calcular qué datos mostrar
   const start = (page - 1) * itemsPerPage;
-  const paginatedData = users.slice(start, start + itemsPerPage);
+  const paginatedData = usersData.slice(start, start + itemsPerPage);
 
   return (
     <>
       <div className="container">
+        {loading ? <p style={{display: "flex", alignContent: "center", textAlign:"center", width:"100%", height: "100%"}}>Cargando usuarios...</p> : (
+          <>
         <div className="search-bar-container">
           <div className="search-bar">
             <i className="fas fa-search"></i>
@@ -76,16 +132,28 @@ const Table_users = () => {
                   <td className="datos-usuario">
                     <div className="nombre-permiso">
                       <p>{users.name}</p>
-                      <span>{users.role}</span>
+                      <span style={{
+                        color: users.role === "Administrador" ? "#4A90E2" : "gray"
+                        }}>{users.role}</span>
                     </div>
                   </td>
-                  <td>
+                  <td style={{textAlign:"center"}}>
                     <p>{users.email}</p>
                   </td>
-                  <td>
+                  <td style={{textAlign:"center"}}>
                     <p>{users.cellphone}</p>
                   </td>
-                  <td>{users.status}</td>
+                  <td style={{textAlign: "center"}}>
+                    <span
+                    style={{
+                      padding: "1rem", 
+                      border: users.status === "Activo" ? "2px solid green" : "2px solid red",
+                      color: users.status === "Activo" ? "green" : "red",
+                      borderRadius: "8px",
+                      minWidth: "max-content"
+                    }}
+                    >{users.status}</span>
+                  </td>
                   <td className="acciones-usuario">
                     <i className="fas fa-edit edit-container"></i>
                     <i className="fas fa-trash-alt delete-container"></i>
@@ -102,6 +170,8 @@ const Table_users = () => {
           />
         </div>
         </div>
+        </> 
+)}
         
       </div>
     </>
