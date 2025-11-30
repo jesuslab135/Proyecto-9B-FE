a1ed656f41e08041f587e589c9720f453a2d78dd
gagor02 <gagor0202@gmail.com>
2025-11-29 22:02:02 -0800

    29/11/2025 funciones de api con usuarioslist, pelea con modal

--- src/components/admin/usuarios_crud_components/Table_users.jsx
+++ src/components/admin/usuarios_crud_components/Table_users.jsx
@@ -1,179 +1,164 @@
 import "./Table_users.css";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Pagination from "../../../components/Global_components/Pagination";
+import { UsuariosAPI } from "../../../utils/api/usuarios.client";
+import { Modal } from "react-responsive-modal";
 
 const Table_users = () => {
+  const [users, setUsers] = useState([]);
 
-  // const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
-  
+  const [loading, setLoading] = useState(true);
+
   const itemsPerPage = 5;
   const [page, setPage] = useState(1);
 
-  const usersData = [
-    {
-      id: 1,
-      name: "Juan Pérez",
-      role: "Consumidor",
-      email: "user@gmail.com", 
-      cellphone: "555-1234",
-      status: "Inactivo"
-    },
-    {
-      id: 2,
-      name: "Juan Pérez",
-      role: "Administrador",
-      email: "user@gmail.com", 
-      cellphone: "555-1234",
-      status: "Activo"
-    }, 
-    {
-      id: 3,
-      name: "Juan Pérez",
-      role: "Administrador",
-      email: "user@gmail.com", 
-      cellphone: "555-1234",
-      status: "Activo"
-    }, 
-    {
-      id: 4,
-      name: "Juan Pérez",
-      role: "Administrador",
-      email: "user@gmail.com", 
-      cellphone: "555-1234",
-      status: "Activo"
-    }, 
-    {
-      id: 5,
-      name: "Juan Pérez",
-      role: "Administrador",
-      email: "user@gmail.com", 
-      cellphone: "555-1234",
-      status: "Activo"
-    }, 
-    {
-      id: 6,
-      name: "Juan Pérez",
-      role: "Administrador",
-      email: "user@gmail.com", 
-      cellphone: "555-1234",
-      status: "Activo"
-    }, 
-    {
-      id: 7,
-      name: "Juan Pérez",
-      role: "Administrador",
-      email: "user@gmail.com", 
-      cellphone: "555-1234",
-      status: "Activo"
-    }
-  ]
+  // const para el Modal
+  const [open, setOpen] = useState(false);
+  const onCloseModal = () => setOpen(false);
+  const onOpenModal = () => setOpen(true);
 
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     try {
-  //       const response = await fetch("http://127.0.0.1:8000/api/usuarios/");
-  //       const json = await response.json();
-  //       setUsers(json);
-  //     } catch (error) {
-  //       console.error("Error fetching users:", error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
+  // Funicon para obtener al lista usuarios
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await UsuariosAPI.list();
+        const ordered = (res.results || []).sort((a, b) => a.id - b.id);
+        setUsers(ordered);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+        setLoading(true);
+      }
+    };
+    fetchUsers();
+  }, []);
 
-  //   fetchUsers();
-  //   // console.log('q se imprime:', json);
-  // }, []);
+  // Funcion para editar al usuario
+
+  // Funcion para eliminar al usuario
 
   // Math.ceil redondea hacia arriba al entero más cercano
-  const totalPages = Math.ceil(usersData.length / itemsPerPage);
+  const totalPages = Math.ceil(users.length / itemsPerPage);
 
   // Calcular qué datos mostrar
   const start = (page - 1) * itemsPerPage;
-  const paginatedData = usersData.slice(start, start + itemsPerPage);
+  const paginatedData = users.slice(start, start + itemsPerPage);
 
   return (
     <>
       <div className="container">
-        {loading ? <p style={{display: "flex", alignContent: "center", textAlign:"center", width:"100%", height: "100%"}}>Cargando usuarios...</p> : (
+        {loading ? (
+          <p
+            style={{
+              display: "flex",
+              alignContent: "center",
+              textAlign: "center",
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            Cargando usuarios...
+          </p>
+        ) : (
           <>
-        <div className="search-bar-container">
-          <div className="search-bar">
-            <i className="fas fa-search"></i>
-            <input
-              type="text"
-              placeholder="Buscar usuario por nombre, email o rol..."
-            />
-          </div>
-          <div className="nvo-usuario-btn">
-            <p>Crear nuevo usuario</p>
-            <i className="fas fa-plus-circle"></i>
-          </div>
-        </div>
-        <div className="table-container">
-          <table className="users-table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Usuario</th>
-                <th>Email</th>
-                <th>Teléfono</th>
-                <th>Estado</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {paginatedData.map((users, index) => (
-                <tr key={index}>
-                  <td>
-                    <p>{users.id}</p>
-                  </td>
-                  <td className="datos-usuario">
-                    <div className="nombre-permiso">
-                      <p>{users.name}</p>
-                      <span style={{
-                        color: users.role === "Administrador" ? "#4A90E2" : "gray"
-                        }}>{users.role}</span>
-                    </div>
-                  </td>
-                  <td style={{textAlign:"center"}}>
-                    <p>{users.email}</p>
-                  </td>
-                  <td style={{textAlign:"center"}}>
-                    <p>{users.cellphone}</p>
-                  </td>
-                  <td style={{textAlign: "center"}}>
-                    <span
-                    style={{
-                      padding: "1rem", 
-                      border: users.status === "Activo" ? "2px solid green" : "2px solid red",
-                      color: users.status === "Activo" ? "green" : "red",
-                      borderRadius: "8px",
-                      minWidth: "max-content"
-                    }}
-                    >{users.status}</span>
-                  </td>
-                  <td className="acciones-usuario">
-                    <i className="fas fa-edit edit-container"></i>
-                    <i className="fas fa-trash-alt delete-container"></i>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className="pagination-container">
-            <Pagination 
-          currentPage={page}
-          totalPages={totalPages}
-          onPageChange={setPage}
-          />
-        </div>
-        </div>
-        </> 
-)}
+            <div className="search-bar-container">
+              <div className="search-bar">
+                <i className="fas fa-search"></i>
+                <input
+                  type="text"
+                  placeholder="Buscar usuario por nombre, email o rol..."
+                />
+              </div>
+              <div className="nvo-usuario-btn">
+                <p>Crear nuevo usuario</p>
+                <i className="fas fa-plus-circle"></i>
+              </div>
+            </div>
+            <div className="table-container">
+              <table className="users-table">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Usuario</th>
+                    <th>Email</th>
+                    <th>Teléfono</th>
+                    <th>Estado</th>
+                    <th>Acciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {paginatedData.map((users, index) => (
+                    <tr key={index}>
+                      <td>
+                        <p>{users.id}</p>
+                      </td>
+                      <td className="datos-usuario">
+                        <div className="nombre-permiso">
+                          <p>{users.nombre}</p>
+                          <span
+                            style={{
+                              color:
+                                users.rol === "Administrador"
+                                  ? "#4A90E2"
+                                  : "gray",
+                            }}
+                          >
+                            {users.rol}
+                          </span>
+                        </div>
+                      </td>
+                      <td style={{ textAlign: "center" }}>
+                        <p>{users.email}</p>
+                      </td>
+                      <td style={{ textAlign: "center" }}>
+                        <p>{users.telefono}</p>
+                      </td>
+                      <td style={{ textAlign: "center" }}>
+                        <span
+                          style={{
+                            padding: "1rem",
+                            // border: users.status === "Activo" ? "2px solid green" : "2px solid red",
+                            // color: users.status === "Activo" ? "green" : "red",
+                            borderRadius: "8px",
+                            minWidth: "max-content",
+                          }}
+                        >
+                          Activo
+                        </span>
+                      </td>
+                      <td className="acciones-usuario">
+                        <button onClick={onOpenModal}>
+                          <i className="fas fa-edit edit-container"></i>
+                        </button>
+
+                        <i className="fas fa-trash-alt delete-container"></i>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <div className="pagination-container">
+                <Pagination
+                  currentPage={page}
+                  totalPages={totalPages}
+                  onPageChange={setPage}
+                />
+              </div>
+            </div>
+          </>
+        )}
         
       </div>
+      <Modal open={open} onClose={onCloseModal} center  container={document.body}>
+        <h2>Simple centered modal</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
+          hendrerit risus, sed porttitor quam.
+        </p>
+      </Modal>
+      
+      
     </>
   );
 };
